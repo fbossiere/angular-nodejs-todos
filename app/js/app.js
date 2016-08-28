e526657b2517c8aeb3b85658f0bc2b8c7103d953
= <=>
2016-08-28 19:42:40 +0200

    init

---
+++ app/js/app.js
@@ -0,0 +1,28 @@
+var scotchTodo = angular.module('scotchTodo', []);
+
+scotchTodo.controller('mainController', ['$scope', '$http', function ($scope, $http) {
+    $scope.formData = {};
+
+    // when landing on the page, get all todos and show them
+    $http.get('/api/todos').then(function(response){
+        $scope.todos = response.data;
+        console.log(response.data);
+    });
+
+    // when submitting the add form, send the text to the node API
+    $scope.createTodo = function () {
+        $http.post('/api/todos', $scope.formData).then(function(response){
+            $scope.formData = {}; // clear the form so our user is ready to enter another
+            $scope.todos = response.data;
+            console.log(response.data);
+        });
+    };
+
+    // delete a todo after checking it
+    $scope.deleteTodo = function (id) {
+        $http.delete('/api/todos/' + id).then(function(response){
+            $scope.todos = response.data;
+            console.log(response.data);
+        });
+    };
+}]);
